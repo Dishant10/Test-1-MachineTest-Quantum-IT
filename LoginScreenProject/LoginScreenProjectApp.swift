5c984c4d6589d2927eba6c2dbd57faf2dafbe58c
Dishant Nagpal <dishantnagpal10@gmail.com>
2022-06-18 15:36:51 +0530

    Initial Commit

---
+++ LoginScreenProject/LoginScreenProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenProjectApp.swift
+//  LoginScreenProject
+//
+//  Created by Dishant Nagpal on 18/06/22.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
